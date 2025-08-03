02dd59d5d7711a918525a25200fdd29d2238254a
Raphael Souza <raphael@Raphaels-MacBook-Air.local>
2025-08-03 01:26:44 -0300

    First commit about macOSDownloader

---
+++ macOSDownloader/macOSDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOSDownloaderApp.swift
+//  macOSDownloader
+//
+//  Created by Raphael Souza on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct macOSDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
